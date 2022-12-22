4e0650661c8d4c54ef4bcde8214d829eedb987fb
Sergei Jeihala <sergejey@gmail.com>
2022-12-22 19:27:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ git clone https://github.com/sergejey/majordomo-rpi-install.git
 cd majordomo-rpi-install
 
 my_dir="$(dirname "$0")"
-"$my_dir/libraries/general.sh"
+bash "$my_dir/libraries/general.sh"
 
 helloWorld
