22d7cc9664b33dc48e74aff5564b1b2e7a107491
Sergei Jeihala <sergejey@gmail.com>
2022-12-22 19:34:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ sudo apt-get upgrade
 sudo apt-get install git
 rm -Rf ./majordomo-rpi-install
 git clone https://github.com/sergejey/majordomo-rpi-install.git
+
 cd majordomo-rpi-install
 
-my_dir="$(dirname "$0")"
-
-source "$my_dir/libraries/general.sh"
+source "./libraries/general.sh"
 
 helloWorld
