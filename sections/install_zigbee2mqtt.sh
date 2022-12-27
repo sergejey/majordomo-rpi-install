56d222c1cf655498ae31644ae708798d35a585a3
Sergei Jeihala <sergejey@gmail.com>
2022-12-27 11:49:13 +0200

    Update install_zigbee2mqtt.sh

--- sections/install_zigbee2mqtt.sh
+++ sections/install_zigbee2mqtt.sh
@@ -11,7 +11,7 @@ sudo echo "\nfrontend:">>/opt/zigbee2mqtt/data/configuration.yaml
 sudo echo "\n  port: 8080">>/opt/zigbee2mqtt/data/configuration.yaml
 sudo echo "\n  host: 0.0.0.0">>/opt/zigbee2mqtt/data/configuration.yaml
 cd /opt/zigbee2mqtt/
-runSudo "npm ci
+runSudo "npm ci"
 
 cd ~/majordomo-rpi-install
 runSudo "cp ./resources/zigbee2mqtt.service /etc/systemd/system/zigbee2mqtt.service"
