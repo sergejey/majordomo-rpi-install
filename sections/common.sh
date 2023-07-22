30a83aa2e07c6ef8a097352166fb783bcf7ed754
Sergei Jeihala <sergejey@gmail.com>
2023-07-22 22:56:07 +0300

    Update common.sh

--- sections/common.sh
+++ sections/common.sh
@@ -12,7 +12,7 @@ showMessage "Updating OS."
 runSudo "apt-get update"
 runSudo "apt-get upgrade -y"
 
-runSudo "apt-get install -y mc mpd mplayer unzip ntp"
+runSudo "apt-get install -y mc mpd mplayer unzip ntp python3 python3-pip"
 runSudo "systemctl enable ssh"
 runSudo "systemctl start ssh"
 runSudo "service ntp restart"
