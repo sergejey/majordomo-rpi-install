23f97e040be885ce8236b8cd717836f7d7c826e1
Sergei Jeihala <sergejey@gmail.com>
2024-12-12 20:14:34 +0200

    Hostname update

--- sections/install_hostname.sh
+++ sections/install_hostname.sh
@@ -7,6 +7,9 @@ runSudo "systemctl enable avahi-daemon.service"
 runSudo "systemctl start avahi-daemon.service"
 
 replaceString "/etc/hostname" "raspberrypi" "$dns_hostname"
+replaceString "/etc/hostname" "raspberry" "$dns_hostname"
+replaceString "/etc/hostname" "raspdesktop" "$dns_hostname"
 replaceString "/etc/hosts" "raspberrypi" "$dns_hostname"
+replaceString "/etc/hosts" "raspdesktop" "$dns_hostname"
 
 showMessage "DNS record installed."
