7d4cf522a0d412328b45604ff0fe957f1665af27
Sergei Jeihala <sergejey@gmail.com>
2025-02-05 20:02:39 +0200

    Zigbee2mqtt installation update

--- sections/install_nodejs.sh
+++ sections/install_nodejs.sh
@@ -2,12 +2,8 @@
 
 showMessage "Installing NodeJS..."
 
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update
-
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 runSudo "apt-get install -y nodejs"
 runSudo "apt-get install -y npm"
-
+node -v
 showMessage "NodeJS installed."
